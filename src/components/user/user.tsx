f5fc2cbe74d6a70e79c30aa7fe354d15ed58c1ea
Saroj Shrestha <codkanxa@gmail.com>
2023-04-10 13:04:31 +0545

    sth

--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -5,7 +5,7 @@ import { BiLogOut } from 'react-icons/bi'
 import useGoogleLogout from '../../hooks/useGoogleLogin/useGoogleLogout'
 import { Link } from 'react-router-dom'
 
-const solutions = [
+const links = [
   {
     name: 'Wishlist',
     description: 'Create your own targeted content',
@@ -78,24 +78,8 @@ export default function User({ photo, name }: Props) {
                     <BiLogOut className="text-xl" />
                   </Popover.Button>
                 </div>
-                {solutions.map((item) => (
-                  // <a
-                  //   key={item.name}
-                  //   href={item.href}
-                  //   className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                  // >
-                  //   <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                  //     <item.icon aria-hidden="true" />
-                  //   </div>
-                  //   <div className="ml-4">
-                  //     <p className="text-sm font-medium text-gray-900">
-                  //       {item.name}
-                  //     </p>
-                  //     <p className="text-sm text-gray-500">
-                  //       {item.description}
-                  //     </p>
-                  //   </div>
-                  // </a>
+                {links.map((item) => (
+  
 
                   <li
                     key={item.name}
